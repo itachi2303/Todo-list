eeae6049695f72c764a9cc2bdfafeb97d731a76e
itachi2303 <149932832+itachi2303@users.noreply.github.com>
2024-05-17 13:50:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+// To do list
+import inquirer from "inquirer";
+let condition = true;
+let tasks = [];
+while (condition) {
+    let addtask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: "Enter your task"
+        },
+        {
+            name: "input2",
+            type: "confirm",
+            message: "Do you want to add more task",
+            default: false
+        },
+        {
+            name: "input3",
+            type: "list",
+            message: "Do you want to delete task",
+            choices: ["yes", "no"],
+        },
+    ]);
+    tasks.push(addtask.task);
+    console.table(tasks);
+    if (addtask.input2 === true) {
+        continue;
+    }
+    else if (addtask.input3 === "yes") {
+        tasks.pop();
+        console.log(tasks);
+    }
+    else {
+        condition = false;
+    }
+}
